b0ecdfb3e66d44d257c5fce253641a3d7b08ecba
frag223 <m.riquelme223@gmail.com>
2025-11-02 08:03:14 +1100

    adding skeleton component

--- src/lib/components/grid/index.ts
+++ src/lib/components/grid/index.ts
@@ -1,4 +1,5 @@
 import Grid from './grid.svelte';
 import GridHeader from './grid_header.svelte';
+import GridSkeleton from './grid-skeleton.svelte';
 
-export { Grid, GridHeader };
+export { Grid, GridHeader, GridSkeleton };
