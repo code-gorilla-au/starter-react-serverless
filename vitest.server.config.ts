87a4ea411cc07290fd509c34b477606b8a8776a1
frag223 <m.riquelme223@gmail.com>
2025-12-30 21:23:18 +1100

    adding ci pipes on PR

--- vitest.server.config.ts
+++ vitest.server.config.ts
@@ -2,7 +2,12 @@ import { defineConfig } from 'vitest/config';
 
 import { shared } from './vite.config';
 
-const exclude = ['app/**/*.client*.{ts,tsx}', 'app/components/**/*.{ts,tsx}', '**/node_modules/**'];
+const exclude = [
+	'app/**/*.client*.{ts,tsx}',
+	'app/components/**/*.{ts,tsx}',
+	'app/routes/**/*.{ts,tsx}',
+	'**/node_modules/**'
+];
 
 export default defineConfig({
 	...shared,
